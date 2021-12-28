465bb377100036920a95c45c802d21ed254462ba
Felipe Parodi <fparodi@pennmedicine.upenn.edu>
2021-12-28 14:49:58 -0500

    Add files via upload

---
+++ png2jpg.py
@@ -0,0 +1,24 @@
+# Convert png in folder to jpg
+
+# Convert png to jpg:
+import os
+import cv2
+import glob
+import argparse
+from PIL import Image
+
+# Get the path to the images:
+parser = argparse.ArgumentParser(description='Convert to jpg.')
+parser.add_argument("-p","--path2imgs", help="Video location", default='/Users/felipeparodi/Desktop/MonkeyImgs/')
+args = parser.parse_args()
+fold = glob.gob(args.path2imgs)
+
+# input_dir = '/Users/felipeparodi/Desktop/MonkeyImgs/'
+# cast to list to avoid error:
+fold = list(glob.glob(input_dir+'*.png'))
+for f in fold:
+    # only jpg files:
+    if os.path.splitext(f)[1] == '.png':
+        img = Image.open(f)
+        img.save(f.replace('.png','.jpg'))
+        os.remove(f)
