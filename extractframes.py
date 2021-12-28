e5b8424a0fe7f8dd0248130560ca3d0427e2b815
Felipe Parodi <fparodi@pennmedicine.upenn.edu>
2021-12-28 14:19:57 -0500

    Add files via upload

--- extractframes.py
+++ extractframes.py
@@ -3,22 +3,25 @@ import cv2
 import glob
 import argparse
 
+# Get the path to the videos:
 parser = argparse.ArgumentParser(description='Frame extraction.')
-
-
 parser.add_argument("-p","--path2vids", help="Video location", default='/Users/felipeparodi/Desktop/MonkeyImgs/*.mp4')
 args = parser.parse_args()
 
-fold = glob.glob('/Users/felipeparodi/Desktop/MonkeyImgs/*.mp4')
+# fold = glob.glob('/Users/felipeparodi/Desktop/MonkeyImgs/*.mp4')
+# Use argument passed by user:
+fold = glob.glob(args.path2vids)
 
-for f in fold:
+# Loop through the videos:
+for idx, f in enumerate(fold):
     cap = cv2.VideoCapture(str(f))
     count=0
     while(cap.isOpened()):
         ret, frame = cap.read()
         if ret:
-            cv2.imwrite('/Users/felipeparodi/Desktop/MonkeyImgs/frame{:d}.jpg'.format(count),frame)
-            print('frame{:d}.jpg'.format(count))
+            # Name frames with the video number and the frame number:
+            cv2.imwrite('/Users/felipeparodi/Desktop/MonkeyImgs/cam{:d}_{:d}.jpg'.format(idx,count),frame)
+            print('cam{:d}_{:d}.jpg'.format(idx,count))
             count += 30 # skip 30 frames
             cap.set(cv2.CAP_PROP_POS_FRAMES, count)
         else:
