c112d8b468063806e3766e4d0c6b7a93745f7c97
Felipe Parodi <fparodi@pennmedicine.upenn.edu>
2021-12-28 16:30:23 -0500

    Add files via upload

--- png2jpg.py
+++ png2jpg.py
@@ -11,14 +11,28 @@ from PIL import Image
 parser = argparse.ArgumentParser(description='Convert to jpg.')
 parser.add_argument("-p","--path2imgs", help="Video location", default='/Users/felipeparodi/Desktop/MonkeyImgs/')
 args = parser.parse_args()
-input_dir = glob.gob(args.path2imgs)
 
-# input_dir = '/Users/felipeparodi/Desktop/MonkeyImgs/'
-# cast to list to avoid error:
-fold = list(glob.glob(input_dir+'*.png'))
-for f in fold:
-    # only jpg files:
-    if os.path.splitext(f)[1] == '.png':
-        img = Image.open(f)
-        img.save(f.replace('.png','.jpg'))
-        os.remove(f)
+# Path to images
+input_dir = glob.gob(args.path2imgs)
+# input_dir = '/Users/felipeparodi/Desktop/MonkeyImgs/*.png'
+names = glob.glob(input_dir)
+
+# Initalize the counter:
+counter = 0
+
+# Loop through the images:
+for name in names:
+    # Get the name of the image:
+    img = Image.open(input_dir + name) # open color image
+    name = name.split(".") # split the filename
+    # Convert to jpg:
+    if name[-1] == "png":
+        name[-1] = "jpg" # replace the extension
+        name = str.join(".", name) # reassemble the filename
+        save_path = input_dir + name # create path to save image
+        img = img.convert('RGB') # convert to RGB
+        img.save(save_path) # save the image
+        counter += 1 # increment counter
+        # os.remove(name) # remove the original image
+    else:
+        continue
